2612cb592367ea0503cfc01a8beee6fc643ea50d
nabyungho <valoon2@naver.com>
2025-04-05 01:11:54 +0900

    [feat] basic type String

---
+++ src/main/kotlin/ch2_strings/s1_strings/S1_Strings.kt
@@ -0,0 +1,14 @@
+package org.example.ch2_strings.s1_strings
+
+class S1_Strings {
+
+    val str = "abcd 123"
+
+    val s = "abc" + 1
+
+    fun strRoof() {
+        for (c in str) {
+            println(c)
+        }
+    }
+}
