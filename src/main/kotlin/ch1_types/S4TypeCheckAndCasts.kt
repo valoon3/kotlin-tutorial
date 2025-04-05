b01ab047705d5b5613a742c663c054b3531c8199
nabyungho <valoon2@naver.com>
2025-04-05 15:56:43 +0900

    [fix] basic type check and casts

---
+++ src/main/kotlin/ch1_types/S4TypeCheckAndCasts.kt
@@ -0,0 +1,18 @@
+package org.example.ch1_types
+
+class S4TypeCheckAndCasts {
+
+    fun isOperator(value: Any): Boolean {
+        return value is String;
+    }
+
+    fun controlFlow(value: Any): String {
+        return when (value) {
+            is String -> "String"
+            is Int -> "Int"
+            is Double -> "Double"
+            else -> "Unknown type"
+        }
+    }
+
+}
