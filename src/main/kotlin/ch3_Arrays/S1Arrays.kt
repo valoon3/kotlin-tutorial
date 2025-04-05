d46be49ed6ce3db4e6ca6710b4c4f7cbb9dcbe4d
nabyungho <valoon2@naver.com>
2025-04-05 15:35:37 +0900

    [feat] basic type Arrays

---
+++ src/main/kotlin/ch3_Arrays/S1Arrays.kt
@@ -0,0 +1,119 @@
+package org.example.ch3_Arrays
+
+class S1Arrays {
+
+    fun whenToUseArrays(): String {
+        var riversArray = arrayOf("Nile", "Amazon", "Yangtze")
+
+        // += 를 사용하는 것을 새로운 객체를 생성
+        riversArray += "Mississippi"
+
+        // Nile, Amazon, Yangtze, Mississippi
+        return riversArray.joinToString()
+    }
+
+    fun createArrays1(): String {
+        // Creates an array with values [1, 2, 3]
+        val simpleArray = arrayOf(1, 2, 3)
+
+        return simpleArray.joinToString()
+    }
+
+    fun createArrays2(): String {
+        // Creates an array with values [null, null, null]
+        val nullArray: Array<Int?> = arrayOfNulls(3)
+
+        // null, null, null
+        return nullArray.joinToString()
+    }
+
+    fun createArrays3(): String {
+        // 배열의 크기만큼 0으로 초기화
+        val initArray = Array<Int>(3) { 0 }
+
+        // 0, 0, 0
+        return initArray.joinToString()
+    }
+
+    fun createArrays(): String {
+        // Array 배열 5 ["0", "1", "4", "9", "16"] 로 초기화
+        val asc = Array(5) { i -> (i * i).toString() }
+
+        // 014916
+        return asc.joinToString()
+    }
+
+    fun nestedArrays() {
+        // 중첩 배열 [[0, 0], [0, 0]]
+        val twoDArray = Array(2) { Array<Int>(2) { 0 } }
+        println(twoDArray.contentDeepToString())
+
+        // Creates a three-dimensional array
+        val threeDArray = Array(3) {
+            Array(3) {
+                Array<Int>(3) { 0 }
+            }
+        }
+        // [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+        println(threeDArray.contentDeepToString())
+    }
+
+    fun numberOfArguments(): String {
+        // vararg 를 통해서 가변 인자 사용이 가능하다
+        fun printAllStrings(vararg strings: String): String {
+            var result: String = ""
+            for (string in strings) result += string
+
+            return result;
+        }
+
+        // 만약 vararg 파라미터가 제네릭 타입일 경우, *을 생략해도 Array<out T> 타입은 인자로 받아들일 수 있다.
+        val lettersArray = arrayOf("c", "d")
+        // abcd
+        return printAllStrings("a", "b", *lettersArray)
+    }
+
+    fun <T> compareArrays(arr1: Array<T>, arr2: Array<T>): Boolean {
+
+        // 두 배열이 동일한 순서로 동일한 요소를 갖는지 비교하려면 .contentEquals () 및 .contentDeepEquals () 함수를 사용하십시오.
+        return arr1.contentEquals(arr2)
+
+        // 평등 (==)과 불평등 (! =) 연산자를 사용하여 배열의 내용을 비교하지 마십시오. 이 연산자는 할당 된 변수가 동일한 객체를 가리키는 지 여부를 확인합니다.
+    }
+
+    fun transformArraysSum(): Int {
+        val sumArray = arrayOf(1, 2, 3)
+
+        return sumArray.sum()
+    }
+
+    fun transformArraysShuffle(): String {
+        // 배열에서 요소를 무작위로 셔플
+        val simpleArray = arrayOf(1, 2, 3)
+        simpleArray.shuffle()
+
+        return simpleArray.joinToString()
+    }
+
+    fun arrConvertList(): List<String> {
+        val simpleArray = arrayOf("a", "b", "c", "c")
+
+        // Converts to a List
+        return simpleArray.toList()
+    }
+
+    fun arrConvertSet(): Set<String> {
+        val simpleArray = arrayOf("a", "b", "c", "c")
+
+        // Converts to a Set
+        return simpleArray.toSet()
+    }
+
+    // todo: 실제 자료형 확인 필요
+    fun arrConvertMap(): Map<String, Int> {
+        val pairArray = arrayOf("apple" to 120, "banana" to 150, "cherry" to 90, "apple" to 140)
+
+        return pairArray.toMap()
+    }
+
+}
---
+++ src/test/kotlin/ch3_Arrays/S1ArraysTest.kt
@@ -0,0 +1,141 @@
+package ch3_Arrays
+
+import org.example.ch3_Arrays.S1Arrays
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class S1ArraysTest {
+    private val s1Arrays = S1Arrays()
+
+    @Test
+    fun whenToUseArrays() {
+        var origin = arrayOf("Nile", "Amazon", "Yangtze")
+        // += 를 사용하면 새로운 객체가 생성되는가
+        var arr = origin + "Mississippi"
+
+        // 서로 다른 객체인가
+        assertEquals(false, origin === arr)
+        // 내부 내용이 다른가
+        assertEquals(false, origin.contentEquals(arr))
+
+        val result = s1Arrays.whenToUseArrays()
+        // += 했을 때 Mississippi 가 잘 포함되는가
+        assertEquals("Nile, Amazon, Yangtze, Mississippi", result)
+    }
+
+    @Test
+    fun createArrays1() {
+        val result = s1Arrays.createArrays1()
+        // 1, 2, 3
+        assertEquals("1, 2, 3", result)
+    }
+
+    @Test
+    fun createArrays2() {
+        val result = s1Arrays.createArrays2()
+        // null, null, null
+        assertEquals("null, null, null", result)
+    }
+
+    @Test
+    fun createArrays3() {
+        val result = s1Arrays.createArrays3()
+        // 0, 0, 0
+        assertEquals("0, 0, 0", result)
+    }
+
+    @Test
+    fun createArrays() {
+        // 배열 초기화
+        val asc = Array(5) { i -> i.toString() }
+        // 0, 1, 2, 3, 4
+        assertEquals("0, 1, 2, 3, 4", asc.joinToString())
+
+        val result = s1Arrays.createArrays()
+        // 0, 1, 4, 9, 16
+        assertEquals("0, 1, 4, 9, 16", result)
+    }
+
+    @Test
+    fun nestedArrays() {
+        // 중첩 배열 [[0, 0], [0, 0]]
+        val twoDArray = Array(2) { Array<Int>(2) { 0 } }
+        assertEquals("[[0, 0], [0, 0]]", twoDArray.contentDeepToString())
+
+        // Creates a three-dimensional array
+        val threeDArray = Array(3) {
+            Array(3) {
+                Array<Int>(3) { 0 }
+            }
+        }
+        assertEquals("[[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]", threeDArray.contentDeepToString())
+    }
+
+    @Test
+    fun numberOfArguments() {
+        // 1, 2, 3
+        val result = s1Arrays.numberOfArguments()
+        assertEquals("abcd", result)
+    }
+
+    @Test
+    fun compareArrays() {
+        val arr1 = arrayOf(1, 2, 3)
+        val arr2 = arrayOf(1, 2, 3)
+        // 서로 다른 객체인가
+        assertEquals(false, arr1 === arr2)
+        // 내부 내용이 같은가
+        assertEquals(true, arr1.contentEquals(arr2))
+    }
+
+    @Test
+    fun transformArraysSum() {
+        val result = s1Arrays.transformArraysSum()
+        // 6
+        assertEquals(6, result)
+    }
+
+    @Test
+    fun transformArraysShuffle() {
+    }
+
+    @Test
+    fun arrConvertList() {
+        val arr = arrayOf(1, 2, 3)
+        // List 로 변환
+        val list = arr.toList()
+        // [1, 2, 3]
+        assertEquals("[1, 2, 3]", list.toString())
+
+        // list 의 타입은 ArrayList 이다
+        assertEquals(ArrayList::class, list::class)
+    }
+
+    @Test
+    fun arrConvertSet() {
+        val arr = arrayOf(1, 2, 3)
+        // Set 으로 변환
+        val set = arr.toSet()
+        // [1, 2, 3]
+        assertEquals("[1, 2, 3]", set.toString())
+
+        // set 의 타입은 HashSet 이다
+        assertEquals(LinkedHashSet::class, set::class)
+    }
+
+    @Test
+    fun arrConvertMap() {
+        val arr = arrayOf("a", "b", "c", "c")
+        // Map 으로 변환
+        val map = arr.groupBy { it }
+
+        // {a=[a], b=[b], c=[c, c]}
+        assertEquals("{a=[a], b=[b], c=[c, c]}", map.toString())
+
+        // map 의 타입은 LinkedHashMap 이다
+        assertEquals(LinkedHashMap::class, map::class)
+
+        assertEquals("{apple=140, banana=150, cherry=90}", s1Arrays.arrConvertMap().toString())
+    }
+}
