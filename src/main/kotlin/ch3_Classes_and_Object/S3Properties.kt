ca5b733dc7f315d77cd6b861ca6cbeeba5344240
nabyungho <valoon2@naver.com>
2025-07-02 16:37:04 +0900

    [feat] class val var
    1. getter setter 에 대한 테스트

--- src/main/kotlin/ch3_Classes_and_Object/S3Properties.kt
+++ src/main/kotlin/ch3_Classes_and_Object/S3Properties.kt
@@ -7,4 +7,17 @@ class S3Properties(var name: String = "Holmes, Sherlock", var street: String = "
 
     // val 은 불변객체로 getter 만 존재한다
     val valProps = listOf("name", "street", "age")
+
+    var money: Int = 1000
+        get() {
+            // field 는 프로퍼티의 backing field 를 나타낸다
+            // field 는 getter setter 에서만 사용할 수 있다
+            return field * 2
+        }
+
+    val pay: Int
+        get() {
+            // getter 재정의
+            return money * 2
+        }
 }
--- src/test/kotlin/ch3_Classes_and_Object/S3PropertiesTest.kt
+++ src/test/kotlin/ch3_Classes_and_Object/S3PropertiesTest.kt
@@ -11,13 +11,26 @@ class S3PropertiesTest {
     @Nested
     inner class PropertiesGetterSetterTest {
         @Test
-        @DisplayName("")
+        @DisplayName("var getter test")
         fun getTest() {
             val S3Properties = S3Properties();
 
             assertEquals("Baker", S3Properties.street)
             assertEquals(10, S3Properties.age)
         }
+
+        @Test
+        @DisplayName("pay getter val 테스트")
+        fun getOverrideTest() {
+            val S3Properties = S3Properties();
+
+            S3Properties.money = 1000
+
+            // getter 재정의
+            assertEquals(2000, S3Properties.pay)
+            assertEquals(2000, S3Properties.money)
+        }
     }
+
 
  }
