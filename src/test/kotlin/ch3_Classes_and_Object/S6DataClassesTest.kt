72fecaa86affc26ae29cc8a5b5c3cb5b81671128
nabyungho <valoon2@naver.com>
2025-07-04 16:49:44 +0900

    [feat] destructuring test

--- src/test/kotlin/ch3_Classes_and_Object/S6DataClassesTest.kt
+++ src/test/kotlin/ch3_Classes_and_Object/S6DataClassesTest.kt
@@ -20,6 +20,21 @@ class S6DataClassesTest {
             assertEquals(user.toString(), copyUser.toString())
         }
 
+        @Test
+        fun dataClassCopyTest() {
+            val user = User("Alice", 30)
+            val copyUser = user.copy(age = user.age + 1)
+
+            assertEquals(copyUser.age, 31)
+        }
+
+        @Test
+        fun destructuringTest() {
+            val user = User("Alice", 30)
+            val (name, age) = user
+            assertEquals("Alice", name)
+            assertEquals(30, age)
+        }
     }
 
 }
