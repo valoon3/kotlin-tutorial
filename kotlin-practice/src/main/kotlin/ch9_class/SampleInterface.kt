8ab6464a960052ca88fa5521a34a43a26d9e1b79
nabyungho <valoon2@naver.com>
2025-09-12 21:44:50 +0900

    [feat] SampleInterface 및 FakeUserRepository 구현 및 테스트 추가

---
+++ kotlin-practice/src/main/kotlin/ch9_class/SampleInterface.kt
@@ -0,0 +1,40 @@
+package ch9_class
+
+class SampleInterface {
+}
+
+class User(val id: Int, val name: String)
+
+interface UserRepository {
+    fun findUser(id: Int): User? =
+        getUsers().find { it.id == id }
+
+    fun addUser(user: User)
+
+    fun getUsers(): List<User>
+}
+
+interface Name {
+    val name: String
+    val surName: String
+}
+
+class FakeUserRepository(
+    override val name: String,
+    override val surName: String
+): UserRepository, Name {
+
+    private var users = mapOf<Int, User>()
+
+    override fun findUser(id: Int): User? {
+        return users[id]
+    }
+
+    override fun addUser(user: User) {
+        users += (user.id to user)
+    }
+
+    override fun getUsers(): List<User> {
+        return users.values.toList()
+    }
+}
---
+++ kotlin-practice/src/test/kotlin/ch9_class/SampleInterfaceTest.kt
@@ -0,0 +1,18 @@
+package ch9_class
+
+import kotlin.test.Test
+
+class SampleInterfaceTest {
+
+    @Test
+    fun 페이크_유저_정상_리파지토리_테스트() {
+        // when
+        val repo: UserRepository = FakeUserRepository("name", "surName")
+        repo.addUser(User(1, "Zed"))
+
+        // then
+        val user = repo.findUser(1)
+        assert(user?.name == "Zed")
+    }
+
+}
