38777ce80496f423d5557c0e70833efbb25dc72a
nabyungho <valoon2@naver.com>
2025-09-13 16:41:42 +0900

    [feat] PracticePizzaFactory 및 관련 테스트 추가
    - PracticePizzaFactory 클래스 생성, 하와이안 및 마게리따 피자 메서드 구현.
    - Ingredient 데이터 클래스 및 피자 토핑 리스트 정의.
    - 하와이안/마게리따 피자 동작 확인용 Unit 테스트 추가.

---
+++ kotlin-practice/src/test/kotlin/ch12_object/PracticePizzaFactoryTest.kt
@@ -0,0 +1,23 @@
+package ch12_object
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PracticePizzaFactoryTest {
+
+    @Test
+    fun 하와이안_피자_테스트() {
+        val expected = listOf("ham", "pineapple")
+        val result = PracticePizzaFactory.hawaiian().toppings
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun 마게리따_피자_테스트() {
+        val expected = listOf("tomato", "mozzarella")
+        val result = PracticePizzaFactory.margherita().toppings
+
+        assertEquals(expected, result)
+    }
+
+}
