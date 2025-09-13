ff4c1ad1292c0402d8f1e009d3d67be06fbb5f01
nabyungho <valoon2@naver.com>
2025-09-13 16:01:15 +0900

    [feat] Object Expression 및 Object Declaration 예제 추가
    - Part1ObjectExpression 및 Part2ObjectDeclaration 클래스 생성.
    - Object Expression과 Object Declaration 동작을 확인하는 테스트 작성.
    - Money 추상 클래스 및 USD 서브클래스 구현, 관련 동작 정의.

---
+++ kotlin-practice/src/main/kotlin/ch12_object/Part2ObjectDeclaration.kt
@@ -0,0 +1,40 @@
+package ch12_object
+
+import java.math.BigDecimal
+import java.math.MathContext
+import java.math.RoundingMode
+
+class Part2ObjectDeclaration {
+    data class User(val name: String)
+
+    interface UserProducer {
+        fun produce(): User
+    }
+
+
+    object FakeUserProducer: UserProducer {
+        override fun produce(): User = User("Fake")
+    }
+
+    fun setUserProducer(producer: UserProducer) {
+        println(producer.produce())
+    }
+}
+
+abstract class Money(
+    val amount: BigDecimal,
+    val currency: String
+)
+
+class USD(amount: BigDecimal): Money(amount, "USD") {
+    companion object {
+        private val MATH = MathContext(2, RoundingMode.HALF_EVEN)
+
+        fun from(amount: Int): USD = USD(BigDecimal(amount, MATH))
+
+        fun from(amount: Double): USD = USD(BigDecimal(amount, MATH))
+
+        @Throws(NumberFormatException::class)
+        fun from(amount: String): USD = USD(amount.toBigDecimal())
+    }
+}
