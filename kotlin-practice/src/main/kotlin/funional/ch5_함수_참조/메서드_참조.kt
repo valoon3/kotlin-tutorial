309143d828d59e124dc0a99730ab75c86918a3f8
nabyungho <valoon2@naver.com>
2025-09-20 15:25:30 +0900

    [feat] 메서드 참조

---
+++ kotlin-practice/src/main/kotlin/funional/ch5_함수_참조/메서드_참조.kt
@@ -0,0 +1,19 @@
+package funional.ch5_함수_참조
+
+data class Number(val num: Int) {
+    fun toFloat(): Float = num.toFloat()
+    fun times(n: Int): Number = Number(num * n)
+}
+
+fun main() {
+    val numberObject = Number(10)
+    // member function reference
+    val float: (Number) -> Float = Number::toFloat
+    // `toFloat` has no parameters, but its function type
+    // needs a receiver of type `Number`
+    println(float(numberObject)) // 10.0
+    val multiply: (Number, Int) -> Number = Number::times
+    println(multiply(numberObject, 4)) // Number(num = 40.0)
+    // `times` has one parameter of type `Int`, but its
+    // function type also needs a receiver of type `Number`
+}
